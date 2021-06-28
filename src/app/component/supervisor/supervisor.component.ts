53e9da2aebc6719f11547f8e7504242fdbb05c0f
yitzhak-malik <izakmalik@gmail.com>
2021-06-28 18:16:11 +0300

    Supervisor

--- src/app/component/supervisor/supervisor.component.ts
+++ src/app/component/supervisor/supervisor.component.ts
@@ -7,9 +7,9 @@ import { HttpServicService } from 'src/app/services/http-servic.service';
   styleUrls: ['./supervisor.component.css']
 })
 export class SupervisorComponent implements OnInit {
-
+  academics:object
   constructor(private http:HttpServicService) {
-    this.http.httpGet('/api/supervisor/getAllAcademics').subscribe((s)=>console.log(s)
+    this.http.httpGet('/api/supervisor/getAllAcademics').subscribe((s)=>{console.log(s);this.academics=s}
     )
    }
 
