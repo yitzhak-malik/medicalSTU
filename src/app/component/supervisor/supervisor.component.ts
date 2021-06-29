a1f49d06ed170474e390dbc0e76644613fec30d9
yitzhak-malik <izakmalik@gmail.com>
2021-06-29 16:58:08 +0300

    Supervisor2

--- src/app/component/supervisor/supervisor.component.ts
+++ src/app/component/supervisor/supervisor.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { academicModel, InternModel } from 'src/app/interfaces/intern-model';
 import { HttpServicService } from 'src/app/services/http-servic.service';
 
 @Component({
@@ -7,16 +8,30 @@ import { HttpServicService } from 'src/app/services/http-servic.service';
   styleUrls: ['./supervisor.component.css']
 })
 export class SupervisorComponent implements OnInit {
-  academics:object[]
+  academics:object[]=[{}]
   academic:String
+  interns:InternModel[]
+  classes:academicModel[]=[{}]
+  class:string
+  CreateNewClass=false;
   constructor(private http:HttpServicService) {
   }
   
   ngOnInit(): void {
     this.http.httpGet<any>('/api/supervisor/getAllAcademics').subscribe((academics)=>{
       this.academics=academics;
-    
+      if(!this.academics[1]){
+        this.academic=this.academics[0]['name']
+        this.getInterns()
+      }
     })
+    
+    
+  }
+  getInterns(){
+    this.http.httpPost<any,any>('/api/supervisor/getInterns',{name:this.academic}).subscribe((interns)=>{
+      this.interns=interns;
+      console.log(interns)})
 
   }
 
