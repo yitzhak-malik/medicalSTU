454254e5e6da9b4ed96857be1bbb24df8cfea966
yitzhak-malik <izakmalik@gmail.com>
2021-07-14 15:28:16 +0300

    'firebasewithgituab'

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+   firebaseConfig:{
+    apiKey: "AIzaSyA2XhOuiDtk-Vt6GXkzG0iUfvbaYBEXr_g",
+    authDomain: "madical-jbh.firebaseapp.com",
+    projectId: "madical-jbh",
+    storageBucket: "madical-jbh.appspot.com",
+    messagingSenderId: "706013495000",
+    appId: "1:706013495000:web:d8dff0dabf98b0ad7721ad",
+    measurementId: "G-3VP5Y0G500"
+  }
 };
